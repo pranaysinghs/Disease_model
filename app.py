9aaa389ea895f5a706c28894005f5e6554d965a9
pranaysinghs <111890170+pranaysinghs@users.noreply.github.com>
2025-01-03 16:22:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,26 +4,28 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 # Set page configuration
-st.set_page_config(page_title="Health Assistant",
+st.set_page_config(page_title="Prediction of Disease Outbreaks",
                    layout="wide",
                    page_icon="🧑‍⚕️")
 
-    
 # getting the working directory of the main.py
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
+try:
+    diabetes_model_path = os.path.join(working_dir, 'saved_models', 'diabetes_model.sav')
+    heart_disease_model_path = os.path.join(working_dir, 'saved_models', 'heart_disease_model.sav')
+    parkinsons_model_path = os.path.join(working_dir, 'saved_models', 'parkinsons_model.sav')
 
-diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
-
-heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
-
-parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
+    diabetes_model = pickle.load(open(diabetes_model_path, 'rb'))
+    heart_disease_model = pickle.load(open(heart_disease_model_path, 'rb'))
+    parkinsons_model = pickle.load(open(parkinsons_model_path, 'rb'))
+except (OSError, IOError, pickle.UnpicklingError) as e:
+    st.error(f"Error loading model: {e}")
 
 # sidebar for navigation
 with st.sidebar:
-    selected = option_menu('Multiple Disease Prediction System',
-
+    selected = option_menu('Prediction of Disease Outbreaks System',
                            ['Diabetes Prediction',
                             'Heart Disease Prediction',
                             'Parkinsons Prediction'],
@@ -31,10 +33,8 @@ with st.sidebar:
                            icons=['activity', 'heart', 'person'],
                            default_index=0)
 
-
 # Diabetes Prediction Page
 if selected == 'Diabetes Prediction':
-
     # page title
     st.title('Diabetes Prediction using ML')
 
@@ -65,100 +65,81 @@ if selected == 'Diabetes Prediction':
     with col2:
         Age = st.text_input('Age of the Person')
 
-
     # code for Prediction
     diab_diagnosis = ''
 
     # creating a button for Prediction
-
     if st.button('Diabetes Test Result'):
+        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]
+        diabetes_prediction = diabetes_model.predict([user_input])
 
-        user_input = [Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
-                      BMI, DiabetesPedigreeFunction, Age]
-
-        user_input = [float(x) for x in user_input]
-
-        diab_prediction = diabetes_model.predict([user_input])
-
-        if diab_prediction[0] == 1:
-            diab_diagnosis = 'The person is diabetic'
+        if diabetes_prediction[0] == 1:
+            diab_diagnosis = "The person has diabetes"
+            st.markdown(f"<h3 style='color: red;'>{diab_diagnosis}</h3>", unsafe_allow_html=True)
         else:
-            diab_diagnosis = 'The person is not diabetic'
-
-    st.success(diab_diagnosis)
+            diab_diagnosis = "The person does not have diabetes"
+            st.markdown(f"<h3 style='color: green;'>{diab_diagnosis}</h3>", unsafe_allow_html=True)
 
 # Heart Disease Prediction Page
 if selected == 'Heart Disease Prediction':
-
     # page title
     st.title('Heart Disease Prediction using ML')
 
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        age = st.text_input('Age')
+        Age = st.text_input('Age')
 
     with col2:
-        sex = st.text_input('Sex')
+        Sex = st.text_input('Sex')
 
     with col3:
-        cp = st.text_input('Chest Pain types')
+        ChestPainType = st.text_input('Chest Pain Type')
 
     with col1:
-        trestbps = st.text_input('Resting Blood Pressure')
+        RestingBP = st.text_input('Resting Blood Pressure')
 
     with col2:
-        chol = st.text_input('Serum Cholestoral in mg/dl')
+        Cholesterol = st.text_input('Cholesterol')
 
     with col3:
-        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        FastingBS = st.text_input('Fasting Blood Sugar')
 
     with col1:
-        restecg = st.text_input('Resting Electrocardiographic results')
+        RestingECG = st.text_input('Resting ECG')
 
     with col2:
-        thalach = st.text_input('Maximum Heart Rate achieved')
+        MaxHR = st.text_input('Max Heart Rate')
 
     with col3:
-        exang = st.text_input('Exercise Induced Angina')
+        ExerciseAngina = st.text_input('Exercise Angina')
 
     with col1:
-        oldpeak = st.text_input('ST depression induced by exercise')
+        Oldpeak = st.text_input('Oldpeak')
 
     with col2:
-        slope = st.text_input('Slope of the peak exercise ST segment')
-
-    with col3:
-        ca = st.text_input('Major vessels colored by flourosopy')
-
-    with col1:
-        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        ST_Slope = st.text_input('ST Slope')
 
     # code for Prediction
     heart_diagnosis = ''
 
     # creating a button for Prediction
-
     if st.button('Heart Disease Test Result'):
+        user_input = [Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope]
+        heart_disease_prediction = heart_disease_model.predict([user_input])
 
-        user_input = [age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]
-
-        user_input = [float(x) for x in user_input]
-
-        heart_prediction = heart_disease_model.predict([user_input])
-
-        if heart_prediction[0] == 1:
-            heart_diagnosis = 'The person is having heart disease'
+        if heart_disease_prediction[0] == 1:
+            heart_diagnosis = "The person has heart disease"
+            st.markdown(f"<h3 style='color: red;'>{heart_diagnosis}</h3>", unsafe_allow_html=True)
         else:
-            heart_diagnosis = 'The person does not have any heart disease'
-
-    st.success(heart_diagnosis)
+            heart_diagnosis = "The person does not have heart disease"
+            st.markdown(f"<h3 style='color: green;'>{heart_diagnosis}</h3>", unsafe_allow_html=True)
 
 # Parkinson's Prediction Page
 if selected == "Parkinsons Prediction":
-
     # page title
-    st.title("Parkinson's Disease Prediction using ML")
+    st.title("Parkinson's Prediction using ML")
 
     col1, col2, col3, col4, col5 = st.columns(5)
 
@@ -233,9 +214,8 @@ if selected == "Parkinsons Prediction":
 
     # creating a button for Prediction    
     if st.button("Parkinson's Test Result"):
-
         user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
-                      RAP, PPQ, DDP,Shimmer, Shimmer_dB, APQ3, APQ5,
+                      RAP, PPQ, DDP, Shimmer, Shimmer_dB, APQ3, APQ5,
                       APQ, DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]
 
         user_input = [float(x) for x in user_input]
@@ -244,7 +224,7 @@ if selected == "Parkinsons Prediction":
 
         if parkinsons_prediction[0] == 1:
             parkinsons_diagnosis = "The person has Parkinson's disease"
+            st.markdown(f"<h3 style='color: red;'>{parkinsons_diagnosis}</h3>", unsafe_allow_html=True)
         else:
             parkinsons_diagnosis = "The person does not have Parkinson's disease"
-
-    st.success(parkinsons_diagnosis)
+            st.markdown(f"<h3 style='color: green;'>{parkinsons_diagnosis}</h3>", unsafe_allow_html=True)
